2e112d1cd707b8eae6e578fa1a59663117d6c3f6
User <User>
2017-11-13 00:21:12 +0300

    UFO game tuturial completed. Roguelike tutorial started. Space harvest game stub created.

---
+++ Space harvest/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float speed;
+
+	void Update () {
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
+
+		transform.position = new Vector3(transform.position.x + speed * moveHorizontal, transform.position.y + speed * moveVertical, transform.position.z);
+	}
+}
---
+++ UFO game [tutorial]/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody2D rb2d;
+	private int count = 0;
+
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
+		countText.text = "0";
+		winText.text = "";
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		rb2d.AddForce(speed * movement);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("PickUp")) {
+			other.gameObject.SetActive(false);
+			count++;
+			countText.text = count.ToString();
+			if (count >= 8) winText.text = "You win!";
+		}
+	}
+}
