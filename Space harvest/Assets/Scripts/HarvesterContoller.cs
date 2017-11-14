d0656c91a67c039e7bd4cba2ddbbe1c82011f4cb
Satoshi-Yoda <Satoshi.Yoda.Register.264@gmail.com>
2017-11-15 01:51:58 +0300

    Space harvest: detecting asteroids by harvester.

---
+++ Space harvest/Assets/Scripts/HarvesterContoller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarvesterContoller : MonoBehaviour
+{
+	private List<GameObject> asteroids = new List<GameObject>();
+
+	void Start () { }
+	
+	void Update() {
+		//Debug.Log(asteroids.Count);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Asteroid") {
+			asteroids.Add(other.gameObject);
+		}
+	}
+}
