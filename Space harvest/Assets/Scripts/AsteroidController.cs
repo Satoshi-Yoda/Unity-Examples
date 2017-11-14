acd4460ed7639ebcbb5a3bd1336776e09ed904d4
Satoshi-Yoda <Satoshi.Yoda.Register.264@gmail.com>
2017-11-15 00:50:27 +0300

    Space harvest: first asteroid generator.

---
+++ Space harvest/Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+	public GameObject[] asteroids_s4;
+	public GameObject[] asteroids_s3;
+	public GameObject[] asteroids_s2;
+	public GameObject[] asteroids_s1;
+	public int minerals;
+
+	void Start() {
+		GameObject[] asteroidPrefabs = asteroids_s1;
+		if (minerals >= 1000) asteroidPrefabs = asteroids_s4;
+		else if (minerals >= 500) asteroidPrefabs = asteroids_s3;
+		else if (minerals >= 200) asteroidPrefabs = asteroids_s2;
+
+		GameObject asteroid = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)];
+		Instantiate(asteroid, transform.position, Quaternion.identity);
+	}
+	
+	void Update() { }
+}
---
+++ Space harvest/Assets/Scripts/AsteroidFieldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidFieldController : MonoBehaviour
+{
+	public int count;
+	public float radius;
+	public GameObject prefab;
+
+	void Start() {
+		for (int i = 0; i < count; i++) {
+			//Vector3 position = new Vector3(Random.Range(-radius, +radius), Random.Range(-radius, +radius), 0.0f);
+			Vector3 position = Random.insideUnitCircle * radius;
+			AsteroidController asteroid = (Instantiate(prefab, position, Quaternion.identity) as GameObject).GetComponent<AsteroidController>();
+			asteroid.minerals = Random.Range(50, 1500);
+		}
+	}
+}
