070418d419236bc61272827d72b9c0524fe44403
Satoshi-Yoda <Satoshi.Yoda.Register.264@gmail.com>
2017-11-14 08:06:24 +0300

    Space shooter: ship movement and background.

---
+++ Space shooter [tutorial]/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float x_min, x_max, z_min, z_max;
+
+	public Vector3 Apply(Vector3 position) {
+		return new Vector3(
+			Mathf.Clamp(position.x, x_min, x_max),
+			0.0f, 
+			Mathf.Clamp(position.z, z_min, z_max)
+		);
+	}
+}
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	void Start () { }
+	
+	void FixedUpdate () {
+		var body = GetComponent<Rigidbody>();
+		body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0.0f, Input.GetAxis("Vertical") * speed);
+		body.position = boundary.Apply(body.position);
+		body.rotation = Quaternion.Euler(0.0f, 0.0f, body.velocity.x * -tilt);
+	}
+}
